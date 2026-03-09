d21b8afca067681184c72dee800eb9b57fb7fcd0
sam-1409 <151430312+sam-1409@users.noreply.github.com>
2026-03-10 01:12:12 +0530

    Initial commit

--- auth/recognize.py
+++ auth/recognize.py
@@ -20,7 +20,7 @@ def AuthenticateFace():
     id = 2  # number of persons you want to Recognize
 
 
-    names = ['', 'Samyak']  # names, leave first empty bcz counter starts from 0
+    names = ['', '<YOUR NAME>']  # names, leave first empty bcz counter starts from 0
 
 
     cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # cv2.CAP_DSHOW to remove warning
